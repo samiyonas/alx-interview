9c574073dcd29ceaa5645f8fec88cb8c8c1c3e52
samiyonas <samuel.x.yonas@gmail.com>
2024-08-12 00:07:51 +0300

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,16 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    triangle = [[1]]
+    if n == 1:
+        return start
+    for i in range(n - 1):
+        x = triangle[i]
+        new_triangle = [1]
+        for i in range(len(x) - 1):
+            el = x[i] + x[i + 1]
+            new_triangle.append(el)
+        new_triangle.append(1)
+        triangle.append(new_triangle)
+
+    return triangle
