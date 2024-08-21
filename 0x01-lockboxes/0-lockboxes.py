23702bf3659809ef6ac469ff0e47149f062543ef
samiyonas <samuel.x.yonas@gmail.com>
2024-08-21 23:29:17 +0300

    major fix

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,18 +5,17 @@
 def canUnlockAll(boxes):
     """ a function that determines if all boxes can be opened """
     stack = {}
-    waitList = []
+    waitList = 0
 
     for i in range(len(boxes)):
-        if i not in stack and i != 0:
-            waitList.append(i)
         for j in range(len(boxes[i])):
             if boxes[i][j] not in stack:
                 stack.update({boxes[i][j]: i})
-    if waitList:
-        for k in waitList:
-            if k not in stack:
-                return False
-        return True
+        if i not in stack:
+            waitList += 1
+    if waitList != 0:
+        return False
     else:
         return True
+
+print(canUnlockAll([[4, 6], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]))
