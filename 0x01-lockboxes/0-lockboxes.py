850d54034e118c177445c2dbe6dbdc3603e64cf6
samiyonas <samuel.x.yonas@gmail.com>
2024-08-21 22:57:09 +0300

    lockboxes stack data structure

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    """ a function that determines if all boxes can be opened """
+    stack = {}
+    waitList = []
+
+    for i in range(len(boxes)):
+        if i not in stack and i != 0:
+            waitList.append(i)
+        for j in range(len(boxes[i])):
+            if boxes[i][j] not in stack:
+                stack.update({boxes[i][j]: i})
+    if waitList:
+        for k in waitList:
+            if k not in stack:
+                return False
+        return True
+    else:
+        return True
