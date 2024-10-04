372a884fe4bb93279845f52d922b8521a3686c5e
samiyonas <samuel.x.yonas@gmail.com>
2024-10-04 08:48:53 -0700

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-""" alx interview question (stacks) """
+'''A module for working with lockboxes.
+'''
 
 
 def canUnlockAll(boxes):
-    """ a function that determines if all boxes can be opened """
-    stack = {}
-    waitList = 0
-
-    for i in range(len(boxes)):
-        for j in range(len(boxes[i])):
-            if boxes[i][j] not in stack:
-                stack.update({boxes[i][j]: i})
-        if i not in stack and i != 0:
-            waitList += 1
-    if waitList != 0:
-        return False
-    else:
-        return True
+    '''Checks if all the boxes in a list of boxes containing the keys
+    (indices) to other boxes can be unlocked given that the first
+    box is unlocked.
+    '''
+    n = len(boxes)
+    seen_boxes = set([0])
+    unseen_boxes = set(boxes[0]).difference(set([0]))
+    while len(unseen_boxes) > 0:
+        boxIdx = unseen_boxes.pop()
+        if not boxIdx or boxIdx >= n or boxIdx < 0:
+            continue
+        if boxIdx not in seen_boxes:
+            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
+            seen_boxes.add(boxIdx)
+    return n == len(seen_boxes)
